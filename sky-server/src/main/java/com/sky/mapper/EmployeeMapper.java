0f02237926304725321dc8979006bf7ab3c30366
huang <hello@qq.com>
2024-10-19 21:28:28 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.Employee;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -15,4 +16,8 @@ public interface EmployeeMapper {
     @Select("select * from employee where username = #{username}")
     Employee getByUsername(String username);
 
+    @Insert("insert into sky_take_out.employee (name, username, password, phone, sex, status," +
+            " create_time, update_time, create_user, update_user,id_number) values " +
+            "(#{name},#{username},#{password},#{phone},#{sex},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser},#{idNumber})")
+    void insert(Employee employee);
 }
