f4f4cb7ba5dde928861ef57429f1e86b996b4897
huang <hello@qq.com>
2024-10-20 19:24:53 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -21,6 +23,7 @@ public interface EmployeeMapper {
     @Insert("insert into sky_take_out.employee (name, username, password, phone, sex, status," +
             " create_time, update_time, create_user, update_user,id_number) values " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser},#{idNumber})")
+    @AutoFill(value = OperationType.INSERT)//OperationType是common里定义的一个枚举
     void insert(Employee employee);
 
     /**
@@ -34,6 +37,7 @@ public interface EmployeeMapper {
      * 根据主键动态修改语句
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
